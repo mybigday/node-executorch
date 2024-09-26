841eb2b1eaea7053a205761970a39e75eff1d35a
Hans <hans.chen@bricks.tools>
2024-09-27 04:31:47 +0800

    chore: update README and comment for post-process script

--- scripts/postneon-dist.js
+++ scripts/postneon-dist.js
@@ -3,9 +3,10 @@ const fs = require('fs');
 let platform = 'unknown';
 let arch = 'unknown';
 
-const content = fs.readFileSync('index.node').slice(0, 256);
+// Parse file header to determine platform and architecture
+const content = fs.readFileSync('index.node').subarray(0, 256);
 
-if (content[0] === 0x7f && content[1] === 0x45 && content[2] === 0x4c && content[3] === 0x46) { // ELF x86_64/aarch64
+if (content[0] === 0x7f && content[1] === 0x45 && content[2] === 0x4c && content[3] === 0x46) { // Linux: ELF x86_64/aarch64
   platform = 'linux';
   if (content[18] === 0xb7) {
     arch = 'arm64';
@@ -14,7 +15,7 @@ if (content[0] === 0x7f && content[1] === 0x45 && content[2] === 0x4c && content
   } else {
     console.error('Unknown ELF arch code:', content[18].toString(16));
   }
-} else if (content[0] === 0x4d && content[1] === 0x5a) { // PE x86_64/aarch64
+} else if (content[0] === 0x4d && content[1] === 0x5a) { // Windows: PE x86_64/aarch64
   const pePos = content.indexOf('PE\0\0');
   platform = 'win32';
   const code = content[pePos + 4] + (content[pePos + 5] << 8);
@@ -25,7 +26,7 @@ if (content[0] === 0x7f && content[1] === 0x45 && content[2] === 0x4c && content
   } else {
     console.error('Unknown PE arch code:', code.toString(16));
   }
-} else if (content[0] === 0xfe && content[1] === 0xed && content[2] === 0xfa && content[3] === 0xce) { // Mach-O x86_64/aarch64
+} else if (content[0] === 0xfe && content[1] === 0xed && content[2] === 0xfa && content[3] === 0xce) { // MacOS: Mach-O x86_64/aarch64
   platform = 'darwin';
   if (content[4] === 0x07) {
     arch = 'x64';
@@ -34,12 +35,15 @@ if (content[0] === 0x7f && content[1] === 0x45 && content[2] === 0x4c && content
   }
 }
 
+// Create the directory for the platform and architecture if it doesn't exist
 if (!fs.existsSync(`bin/${platform}/${arch}`)) {
   fs.mkdirSync(`bin/${platform}/${arch}`, { recursive: true });
 }
 
+// Rename the index.node file to executorch.node
 fs.renameSync('index.node', `bin/${platform}/${arch}/executorch.node`);
 
+// Copy the shared libraries to the bin directory
 const installPrefix = process.env.EXECUTORCH_INSTALL_PREFIX || 'executorch/cmake-out';
 
 const shared_libs = ['libextension_module', 'qnn_executorch_backend'];
