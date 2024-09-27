ae5e6859dccd7cf467759f145eae8144875a4b7e
Hans <hans.chen@bricks.tools>
2024-09-27 14:02:36 +0800

    ci: fix Linux aarch64 build

--- cmake/aarch64-linux-gnu.clang.toolchain.cmake
+++ cmake/aarch64-linux-gnu.clang.toolchain.cmake
@@ -3,6 +3,10 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 set(CMAKE_C_COMPILER "clang")
 set(CMAKE_CXX_COMPILER "clang++")
+set(CMAKE_AR "aarch64-linux-gnu-ar")
+set(CMAKE_RANLIB "aarch64-linux-gnu-ranlib")
+set(CMAKE_STRIP "aarch64-linux-gnu-strip")
+set(CMAKE_LINKER "aarch64-linux-gnu-ld")
 
 set(CMAKE_C_FLAGS "-march=armv8-a -target aarch64-linux-gnu")
 set(CMAKE_CXX_FLAGS "-march=armv8-a -target aarch64-linux-gnu")
