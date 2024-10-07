5192e011e260ee8924f4db914cbb62548cea8751
Hans <hans.chen@bricks.tools>
2024-10-08 01:39:20 +0800

    feat: complete build cmake in rust and make Windows link stdc++ statically

--- build.rs
+++ build.rs
@@ -1,8 +1,18 @@
-extern crate build_target;
 extern crate cpp_build;
-use std::path;
-use build_target::{Arch, Os};
-use cmake::Config;
+use std::path::Path;
+use std::fs;
+
+fn link_shared(lib: &str) {
+    println!("cargo:rustc-link-lib=dylib={}", lib);
+}
+
+fn link_whole(lib: &str) {
+    println!("cargo:rustc-link-lib=static:+whole-archive={}", lib);
+}
+
+fn link(lib: &str) {
+    println!("cargo:rustc-link-lib=static={}", lib);
+}
 
 fn main() {
     println!("cargo:rerun-if-changed=executorch/version.txt");
@@ -21,70 +31,73 @@ fn main() {
         _ => panic!("Unsupported arch"),
     };
 
-    // // let executorch = cmake::build(".");
-    // let mut et = Config::new("executorch");
-    // // debug flags
-    // if std::env::var("PROFILE").unwrap() == "debug" {
-    //     et.define("EXECUTORCH_ENABLE_LOGGING", "ON");
-    //     et.define("EXECUTORCH_BUILD_XNNPACK", "ON");
-    //     et.define("EXECUTORCH_BUILD_EXTENSION_DATA_LOADER", "ON");
-    //     et.define("EXECUTORCH_BUILD_EXTENSION_MODULE", "ON");
-    // } else {
-    //     et.define("EXECUTORCH_BUILD_EXTENSION_DATA_LOADER", "ON");
-    //     et.define("EXECUTORCH_BUILD_EXTENSION_MODULE", "ON");
-    //     et.define("EXECUTORCH_BUILD_KERNELS_CUSTOM", "ON");
-    //     et.define("EXECUTORCH_BUILD_KERNELS_QUANTIZED", "ON");
-    //     et.define("EXECUTORCH_BUILD_KERNELS_OPTIMIZED", "ON");
-    //     et.define("EXECUTORCH_BUILD_EXTENSION_TENSOR", "ON");
-    //     et.define("EXECUTORCH_BUILD_CPUINFO", "ON");
-    //     et.define("EXECUTORCH_BUILD_XNNPACK", "ON");
-    //     et.define("EXECUTORCH_BUILD_PTHREADPOOL", "ON");
-    //     et.define("EXECUTORCH_BUILD_SDK", "ON");
-    //     et.define("EXECUTORCH_ENABLE_LOGGING", "ON");
-    //     if os == Os::Windows && arch == Arch::AARCH64 {
-    //         if let Ok(qnn_sdk_root) = std::env::var("QNN_SDK_ROOT") {
-    //             et.define("QNN_SDK_ROOT", qnn_sdk_root);
-    //             et.define("EXECUTORCH_BUILD_QNN", "ON");
-    //         }
-    //     }
-    //     if os == Os::MacOs {
-    //         et.define("EXECUTORCH_BUILD_COREML", "ON");
-    //     }
-    // }
-    // // resolve toolchain file absolute path
-    // // project_root::get_project_root().unwrap() + cmake/mingw-w64-aarch64.clang.toolchain.cmake
-    // let toolchain_file = Path::new(&project_root::get_project_root().unwrap()).join("cmake/mingw-w64-aarch64.clang.toolchain.cmake");
-    // et.define("CMAKE_TOOLCHAIN_FILE", toolchain_file.display().to_string());
-    // let et_path = et.build();
-    // println!("cargo:rustc-link-search=native={}/lib", et_path.display());
+    let install_path = cmake::build(".");
+    println!("cargo:rustc-link-search=native={}/lib", install_path.display());
 
-    let build_script = path::Path::new(&project_root::get_project_root().unwrap()).join("scripts/build.sh");
-    assert!(build_script.exists(), "build.sh not found");
-    let build_script_path = build_script.to_str().unwrap();
-    let out_dir = path::absolute("build").unwrap().display().to_string();
-
-    // if env has no EXECUTORCH_INSTALL_PREFIX, run scripts/build.sh
-    if std::env::var("EXECUTORCH_INSTALL_PREFIX").is_err() {
-        let mut cmd = std::process::Command::new(build_script_path);
-        cmd.args(&[node_platform, node_arch, &out_dir]);
-        cmd.status().unwrap();
+    if node_platform == "win32" {
+        link("c++");
+        link("c++abi");
+        link("unwind");
     }
 
-    let et_path = std::env::var("EXECUTORCH_INSTALL_PREFIX").unwrap_or_else(|_| out_dir);
+    // common
+    link_whole("executorch");
+    link("executorch_core");
+    link("extension_module_static");
+    link("extension_data_loader");
+    link("tokenizer");
 
-    println!("cargo:rustc-link-search=native={}/lib", et_path);
+    // XNNPACK
+    link_whole("xnnpack_backend");
+    link("XNNPACK");
+    link("microkernels-prod");
+    link("cpuinfo");
+    link("pthreadpool");
 
-    let et_all = Config::new(".")
-        .define("EXECUTORCH_INSTALL_PATH", et_path)
-        .build();
-    println!("cargo:rustc-link-search=native={}/lib", et_all.display());
-    println!("cargo:rustc-link-lib=static=executorch_all");
+    if cfg!(debug_assertions) {
+        link_whole("portable_ops_lib");
+        link("portable_kernels");
+    } else {
+        link("extension_tensor");
+        link("extension_threadpool");
 
-    // let install_prefix = std::env::var("EXECUTORCH_INSTALL_PREFIX")
-    //     .unwrap_or_else(|_| "executorch/cmake-out".to_string());
-    // let lib_path = Path::new(&install_prefix).join("lib");
+        // Optimized Ops
+        link_whole("optimized_native_cpu_ops_lib");
+        link("optimized_kernels");
+        link("portable_kernels");
+        link("eigen_blas");
 
-    // println!("cargo:rustc-link-search=native={}", lib_path.display());
+        // Quantized Ops
+        link_whole("quantized_ops_lib");
+        link("quantized_kernels");
+
+        // Custom Ops
+        link_whole("custom_ops");
+
+        // XNNPACK extra
+        if node_platform != "darwin" && node_arch == "arm64" {
+            link_whole("kleidiai");
+        }
+
+        // CoreML
+        if node_platform == "darwin" {
+            link_whole("coremldelegate");
+            link("sqlite3");
+            println!("cargo:rustc-link-arg=-framework Foundation");
+            println!("cargo:rustc-link-arg=-framework CoreML");
+            println!("cargo:rustc-link-arg=-framework Accelerate");
+        }
+
+        // QNN
+        if node_platform == "win32" && node_arch == "arm64" && std::env::var("QNN_SDK_ROOT").is_ok() {
+            println!("cargo:rerun-if-env-changed=QNN_SDK_ROOT");
+            link_shared("qnn_executorch_backend");
+            let src = Path::new(&install_path).join("lib/qnn_executorch_backend.dll");
+            let out_dir = std::env::var("OUT_DIR").unwrap_or_else(|_| ".".to_string());
+            let dst = Path::new(&out_dir).join("bin").join(node_platform).join(node_arch).join("qnn_executorch_backend.dll");
+            fs::copy(src, dst).unwrap();
+        }
+    }
 
     // for nodejs/electron usage
     println!(
